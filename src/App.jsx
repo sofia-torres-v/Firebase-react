c21ac821f5cb5bc99cc18c7e6c4e0ed3bbf6c6c4
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-01-02 16:07:20 -0500

    ADD:setting up environment with react and material ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Auth from "./components/auth/Auth";
+
+function App() {
+    return (
+        <div>
+            <Auth />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+const Auth = () => {
+    return (
+        <div>
+            <Typography variant="h5" color="initial">
+                Auth component
+            </Typography>
+        </div>
+    );
+};
+
+export default Auth;
