32d0e5899719b16d0c12f1ea88d57b0875edc1f4
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-01-02 21:13:58 -0500

    ADD: login and register component with their respective contact forms

---
+++ src/components/RegisterScreen/Register.jsx
@@ -0,0 +1,55 @@
+import { Box, Button, Paper, TextField } from "@mui/material";
+
+import { Typography } from "@mui/material";
+
+const RegisterScreen = () => {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                border: "1px solid red",
+            }}
+        >
+            <Paper
+                elevation={2}
+                sx={{
+                    p: 5,
+                    // border: "1px solid blue",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    gap: 2,
+                    width: "400px",
+                }}
+            >
+                <Typography
+                    variant="h4"
+                    color="primary"
+                    sx={{ textTransform: "uppercase", fontWeight: "bold" }}
+                >
+                    Register
+                </Typography>
+                <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
+                />
+                <TextField
+                    fullWidth
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                />
+                <Button fullWidth variant="contained">
+                    Click me
+                </Button>
+            </Paper>
+        </Box>
+    );
+};
+
+export default RegisterScreen;
