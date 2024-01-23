a80f27e8c936ec01a6db5c2970ac698491a6bd9f
sofiauldemiaclear <sofy.torresv@gmail.com>
2024-01-23 14:53:15 -0500

    ADD: field for the full name and a select to choose to register as a seller or buyer in the form

--- src/components/RegisterScreen/Register.jsx
+++ src/components/RegisterScreen/Register.jsx
@@ -1,4 +1,14 @@
-import { Box, Button, Paper, TextField } from "@mui/material";
+import {
+    Box,
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField,
+} from "@mui/material";
 import { Typography } from "@mui/material";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -6,61 +16,74 @@ import { ToastContainer } from "react-toastify";
 import { toastIt, validateEmail } from "../../utils/utils";
 import { getDocumentByProperty, saveDocument } from "../../actions/actions";
 import moment from "moment";
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import CustomLoading from "../common/CustomLoading";
 
-
 const RegisterScreen = () => {
-    const [loading,setLoading] = useState(false);
-    const [formData, setFormData]= useState({
-        email: '',
-        password:'',
-        confirmPassword:'',
-    })
-
-    const navigate = useNavigate()
-    const handleChange =((event)=>{
-        const { name, value }   = event.target;
-        setFormData ({...formData,[name]: value});
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        fullName:'',
+        email: "",
+        password: "",
+        confirmPassword: "",
+        userRole:'buyer'
     });
 
-    const handleRegister = async ()=>{
-        const{ email, password, confirmPassword} = formData;
+    const navigate = useNavigate();
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleRegister = async () => {
+        const { fullName, email, password, userRole, confirmPassword } = formData;
         if (!validateEmail(email)) {
-            toastIt('Ingrese email válido por favor.', 3);
-            return
+            toastIt("Ingrese email válido por favor.", 3);
+            return;
         }
 
-        if (!password || !confirmPassword) {
-            toastIt('Porfavor introduzca todos los campos', 3);
-            return
+        if (!password || !confirmPassword || !fullName) {
+            toastIt("Porfavor introduzca todos los campos", 3);
+            return;
         }
 
-        console.log('funcionó')
+        console.log("funcionó");
         if (password !== confirmPassword) {
-            toastIt('Las contraseñas no coinciden', 3
-            );
+            toastIt("Las contraseñas no coinciden", 3);
             return;
         }
 
         //instalamos bcrypt para encriptar la contraseña
-        bcrypt.hash(password,10, async(err, hash)=>{
+        bcrypt.hash(password, 10, async (err, hash) => {
             if (err) {
-                console.log('Ocurrió un error:',err)
-            }else{
-                const docs = await getDocumentByProperty('Users', 'email', email);
+                console.log("Ocurrió un error:", err);
+            } else {
+                setLoading(true);
+                const docs = await getDocumentByProperty(
+                    "Users",
+                    "email",
+                    email
+                );
                 if (docs.length > 0) {
-                    toastIt('Este usuario ya existe',
-                     2
-                    );
+                    toastIt("Este usuario ya existe", 2);
                     return;
                 }
 
-                setLoading(true);
-                await saveDocument('Users', { email, hash, date: moment().format()})
+                await saveDocument("Users", {
+                    fullName,
+                    email,
+                    hash,
+                    userRole,
+                    date: moment().format(),
+                });
                 setLoading(false);
-                //Redirigiendo al usuarios registrado a Login
-                navigate('/Login')
+                toastIt("Registro exitoso. Redirigiendo a Login.", 1);
+
+                //Para que demore el redirigir y poder ver el mensaje
+                setTimeout(() => {
+                    //Redirigiendo al usuarios registrado a Login
+                    navigate("/Login");
+                }, 3000);
             }
         });
     };
@@ -93,6 +116,16 @@ const RegisterScreen = () => {
                 >
                     Register
                 </Typography>
+
+                <TextField
+                    name="fullName"
+                    fullWidth
+                    value={formData.fullName}
+                    id="email"
+                    label="Full Name"
+                    variant="outlined"
+                    onChange={handleChange}
+                />
                 <TextField
                     name="email"
                     fullWidth
@@ -122,8 +155,34 @@ const RegisterScreen = () => {
                     variant="outlined"
                     onChange={handleChange}
                 />
+
+                <FormControl sx={{alignSelf: 'start'}}>
+                    <FormLabel id="demo-radio-buttons-group-label">
+                        Gender
+                    </FormLabel>
+                    <RadioGroup
+                        name="userRole"
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="buyer"
+                        value={formData.userRole}
+                        onChange={handleChange}
+                    >
+                        <FormControlLabel
+                            value="buyer"
+                            control={<Radio />}
+                            label="Buyer"
+                        />
+                       
+                        <FormControlLabel
+                            value="seller"
+                            control={<Radio />}
+                            label="Seller"
+                        />
+                    </RadioGroup>
+                </FormControl>
+
                 <Button fullWidth variant="contained" onClick={handleRegister}>
-                    Click me
+                    Register
                 </Button>
                 <Box
                     sx={{
